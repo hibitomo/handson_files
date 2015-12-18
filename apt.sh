d24c78bb0b9105a74a2444a8c075aa446eb22ca0
hibitomo <hibi.tomoya@lab.ntt.co.jp>
2015-12-18 10:21:35 +0900

    Add apt.sh

---
+++ apt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install unzip build-essential libexpat1-dev libgmp-dev libncurses5-dev libssl-dev libpcap-dev byacc flex libreadlinedev python-dev python-pastedeploy python-paste python-twisted git python-setuptools pythonpip libxml2-dev libxslt1-dev ethtool
